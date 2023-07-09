4d81fba2f18b8b9d6f90e937807be99211e0b3fb
feng.ji <feng.ji@accenture.com>
2023-07-09 14:25:41 +0200

    run on all addr port 8080

--- app/start.sh
+++ app/start.sh
@@ -3,4 +3,4 @@
 export GOOGLE_APPLICATION_CREDENTIALS="serviceAccount.json"
 export SENTRY_SDK_DSN="https://36508a31dbb3468994b7ae470704ce10@o718840.ingest.sentry.io/5783806"
 
-uvicorn main:app --reload
+uvicorn main:app --host 0.0.0.0 --port 8080 --reload
